98e8ad02e3343d731519722f9231d518bca8d3ba
Jade Beresford <jade_fletcher@hotmail.co.uk>
2019-11-13 15:15:54 +0000

    add working? method test and pass to docking_station_spec.rb

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,17 @@
 require "docking_station"
-
+require "bike"
 
 describe DockingStation do
-  it "responds to the method release_bike" do
-    expect(subject).to respond_to(:release_bike)
-  end  
-end
 
+  describe "#release_bike" do
+    it "responds to the method release_bike" do
+      expect(subject).to respond_to(:release_bike)
+    end
+
+    it "returns true if working? method is called" do
+      expect((subject.release_bike).working?).to eq true
+    end
+
+  end
+
+end
